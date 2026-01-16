7e7edce87298033a84d054037da7af19456c287a
stingray82 <11146912+stingray82@users.noreply.github.com>
2026-01-16 21:04:56 +0000

    Enqueue parent and child theme stylesheets

--- etch-child-theme/functions.php
+++ etch-child-theme/functions.php
@@ -10,4 +10,26 @@
 // Use the below to include all your other files
 require_once get_stylesheet_directory() . '/inc/dist/uupd/update-function.php';
 
+// Add Ours and Etch's stylesheet
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_and_child_styles' );
+function enqueue_parent_and_child_styles() {
+
+	// Parent theme stylesheet
+	wp_enqueue_style(
+		'parent-style',
+		get_template_directory_uri() . '/style.css'
+	);
+
+	// Child theme stylesheet
+	wp_enqueue_style(
+		'child-style',
+		get_stylesheet_uri(),
+		array( 'parent-style' ),
+		wp_get_theme()->get( 'Version' )
+	);
+}
+
+
+
+
 // Add your own customizations here.
